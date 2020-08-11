d9c326c683d278f00a9d5e5e468ee940c5a697b1
nchuxw <1601325@stu.nchu.edu.cn>
2020-08-11 17:50:47 +0800

    review linear regression

--- deep-learning/02_base/01_linear_regression.py
+++ deep-learning/02_base/01_linear_regression.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 import random
 
 num_inputs = 2
-num_examples = 30
+num_examples = 1000
 true_w = [2, -3.4]
 true_b = 4.2
 features = nd.random.normal(scale=1, shape=(num_examples, num_inputs))
@@ -32,7 +32,24 @@ def data_iter(features, labels, batch_size):
 
 w = nd.random.normal(scale=0.01, shape=(num_inputs, 1))
 b = nd.zeros(shape=(1,))
-w.attach_grad()
-b.attach_grad()
+# print(nd.dot(features, w) + b)
 
-print(nd.dot(features, w) + b)
+lr = 0.03
+num_epochs = 3
+batch_size = 10
+for epoch in range(num_epochs):
+	for x, y in data_iter(features, labels, batch_size):
+		w.attach_grad()
+		b.attach_grad()
+		with autograd.record():
+			y_hat = nd.dot(x, w) + b
+			loss = (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+		loss.backward()
+		w = w - lr * w.grad / batch_size
+		b = b - lr * b.grad / batch_size
+	y_hat = nd.dot(features, w) + b
+	train_loss = (y_hat - labels.reshape(y_hat.shape)) ** 2 / 2
+	print('epoch %d, train_loss=%f' % (epoch + 1, train_loss.mean().asnumpy()))
+
+print(true_w, w)
+print(true_b, b)
